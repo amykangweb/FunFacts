f1faff64d8b557babf541eecbfa4e46f4c269474
Amy Kang <amykang@Amys-MacBook-Air.local>
2017-05-05 23:47:21 -0700

    Initial Commit

---
+++ FunFacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunFacts
+//
+//  Created by Amy Kang on 5/5/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Amy Kang on 5/5/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
